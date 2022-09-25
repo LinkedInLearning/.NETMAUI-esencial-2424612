35a24519aa01cd9b30fd1eea5764a61105422a77
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-25 13:19:17 +0200

    Commit inicial

---
+++ src/WisdomPetMedicine/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using WisdomPetMedicine.DataAccess;
+
+namespace WisdomPetMedicine;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		var dbContext = new WpmDbContext();
+		categories.Text = dbContext.Categories.Count().ToString();
+		products.Text = dbContext.Categories.Count().ToString();
+		clients.Text = dbContext.Clients.Count().ToString();
+	}
+}
---
+++ src/WisdomPetMedicine/MauiProgram.cs
@@ -0,0 +1,24 @@
+﻿using WisdomPetMedicine.DataAccess;
+
+namespace WisdomPetMedicine;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		var dbContext = new WpmDbContext();
+		dbContext.Database.EnsureCreated();
+		dbContext.Dispose();
+
+		return builder.Build();
+	}
+}
