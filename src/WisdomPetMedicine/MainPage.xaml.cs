713ae8883f1e3aa1ac58141704159e8c550eb807
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-25 15:44:41 +0200

    04_06f

--- src/WisdomPetMedicine/MainPage.xaml.cs
+++ src/WisdomPetMedicine/MainPage.xaml.cs
@@ -7,11 +7,6 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-
-		var dbContext = new WpmDbContext();
-		categories.Text = dbContext.Categories.Count().ToString();
-		products.Text = dbContext.Categories.Count().ToString();
-		clients.Text = dbContext.Clients.Count().ToString();
 	}
 
 	private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
