0bb186df22335fe1e975d8c1ff6e0a6160778843
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-26 15:01:09 +0200

    06_01

--- src/WisdomPetMedicine/Views/VisitsPage.xaml.cs
+++ src/WisdomPetMedicine/Views/VisitsPage.xaml.cs
@@ -6,4 +6,15 @@ public partial class VisitsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		var data = Resources["data"] as VisitsData;
+		data.RemainingVisits = 1;
+	}
+}
+
+public class VisitsData
+{
+	public int RemainingVisits { get; set; }
 }
