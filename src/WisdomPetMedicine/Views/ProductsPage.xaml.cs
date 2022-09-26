2ec6d3118628348eaa86f76d72def309ed9ef40d
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-26 17:38:29 +0200

    06_09f

--- src/WisdomPetMedicine/Views/ProductsPage.xaml.cs
+++ src/WisdomPetMedicine/Views/ProductsPage.xaml.cs
@@ -8,15 +8,6 @@ public partial class ProductsPage : ContentPage
 	{
 		InitializeComponent();
 		var dbContext = new WpmDbContext();
-		foreach (var item in dbContext.Products)
-		{
-			var btn = new Button() { Text = item.Name };
-			btn.Clicked += async (s, a) =>
-			{
-				var uri = $"{nameof(ProductDetailsPage)}?id={item.Id}";
-                await Shell.Current.GoToAsync(uri);
-			};
-            data.Children.Add(btn);
-		} 
+		products.ItemsSource = dbContext.Products;
 	}
 }
