a8daf8c368f08038bdb8d4caa8d9aab995bba769
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-26 15:50:18 +0200

    06_05

--- src/WisdomPetMedicine/Views/VisitDetailsPage.xaml.cs
+++ src/WisdomPetMedicine/Views/VisitDetailsPage.xaml.cs
@@ -1,9 +1,14 @@
 namespace WisdomPetMedicine.Views;
 
-public partial class VisitDetailsPage : ContentPage
+public partial class VisitDetailsPage : ContentPage, IQueryAttributable
 {
 	public VisitDetailsPage()
 	{
 		InitializeComponent();
+	}
+
+	public void ApplyQueryAttributes(IDictionary<string, object> query)
+	{
+		Title = $"Cliente: {query["id"]}";
 	}
 }
--- src/WisdomPetMedicine/Views/VisitsPage.xaml.cs
+++ src/WisdomPetMedicine/Views/VisitsPage.xaml.cs
@@ -49,9 +49,35 @@ public class VisitsData : BindableObject
             }
 		}
 	}
+
+	private Client selectedClient;
+
+	public Client SelectedClient
+	{
+		get { return selectedClient; }
+		set { 
+			if (selectedClient != value)
+			{
+                selectedClient = value;
+                RaisePropertyChanged();
+            }
+		}
+	}
+
 	public VisitsData()
 	{
 		var db = new WpmDbContext();
 		Clients = new ObservableCollection<Client>(db.Clients);
+		PropertyChanged += VisitsData_PropertyChanged;
+	}
+
+	private async void VisitsData_PropertyChanged(object sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName == nameof(SelectedClient))
+		{
+			var uri 
+				= $"{nameof(VisitDetailsPage)}?id={SelectedClient.Id}";
+            await Shell.Current.GoToAsync(uri);
+		}
 	}
 }
