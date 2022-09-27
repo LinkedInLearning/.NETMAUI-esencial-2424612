f4eb7fbcbf4e0539427bef3d2122b5f94dfb011b
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2022-09-27 15:52:17 +0200

    11_01f

---
+++ src/WisdomPetMedicine/DataAccess/WpmOutDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WisdomPetMedicine.DataAccess;
+public class WpmOutDbContext : DbContext
+{
+    public DbSet<SaleItem> Sales { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
+
+public record SaleItem(int ClientId, 
+    int ProductId, int Quantity, decimal Price);
